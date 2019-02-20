4191f14e25ceed211ecc9eae7375bbbb18cabcce
BRIUG <michaelyangbo@outlook.com>
2019-02-20 11:31:40 +0800

    self-organizing map.

---
+++ som/src/distance.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+'''
+ Name: Michael Young
+ Email: michaelyangbo@outlook.com
+ Date: 2019/2/20
+'''
+
+import numpy as np
+
+def select_closest(candidates, origin):
+    """Return the index of the closest candidate to a given point."""
+    return euclidean_distance(candidates, origin).argmin()
+
+def euclidean_distance(a, b):
+    """Return the array of distances of two numpy arrays of points."""
+    return np.linalg.norm(a - b, axis=1)
+
+def route_distance(cities):
+    """Return the cost of traversing a route of cities in a certain order."""
+    points = cities[['x', 'y']]
+    distances = euclidean_distance(points, np.roll(points, 1, axis=0))
+    return np.sum(distances)
